3e4009633cf7c18e4ef0548d029fda84f7d76230
Marco Primi <mprimi@apple.com>
2015-05-25 09:34:06 -0700

    Freshly generated scrapy project

---
+++ scrapy/hangrygoat/hangrygoat/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for hangrygoat project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'hangrygoat'
+
+SPIDER_MODULES = ['hangrygoat.spiders']
+NEWSPIDER_MODULE = 'hangrygoat.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'hangrygoat (+http://www.yourdomain.com)'
