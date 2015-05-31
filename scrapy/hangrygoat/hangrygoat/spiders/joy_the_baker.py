da1363fe8d52cfb22e0e6b938e535b238d5869d0
Marco Primi <mprimi@apple.com>
2015-05-31 16:37:59 -0700

    Renaming first spider to Joy The Baker

--- scrapy/hangrygoat/hangrygoat/spiders/joy_the_baker.py
+++ scrapy/hangrygoat/hangrygoat/spiders/joy_the_baker.py
@@ -4,18 +4,17 @@ from hangrygoat.items import HangrygoatItem
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 
-class BlogTitleSpider(CrawlSpider):
-    name = "blog_title"
+class JoyTheBakerSpider(CrawlSpider):
+    name = "joy_the_baker"
     allowed_domains = ["joythebaker.com"]
     start_urls = [
-        "http://joythebaker.com/2015/05/let-it-be-sunday-21/"
+        "http://joythebaker.com/"
     ]
-    
+
     rules = [
-        #Rule(LinkExtractor(deny = ('.*comment-page.*') )), 
         Rule(LinkExtractor(allow = '^http://joythebaker.com/page/\d+/$')),
         Rule(LinkExtractor(allow = '^http://joythebaker.com/\d\d\d\d/\d\d/.*/$'), callback='parse_item', follow=True)
-    ]        
+    ]
 
 
     """
@@ -27,9 +26,4 @@ class BlogTitleSpider(CrawlSpider):
         item['timestamp'] = response.xpath('//span[@class="posted-on"]/a/time[1]/@datetime').extract()
         item['url'] = response.xpath('/html/head/link[@rel="canonical"]/@href').extract()
         item['raw_content'] = response.xpath('//div[@class="entry-content"]/*[not(self::div)]/text()').extract()
-        item['raw_recipe'] = response.xpath('//div[@class="entry-content"]//div[@class="blog-yumprint-recipe-contents"]').extract()
         yield item
-
-        # filename = response.url.split("/")[-2]    
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
